be335d5969578afc9c76afc5adac1ca08f17ab2d
adelajahalima <adelajahalima@gmail.com>
2024-08-12 19:00:15 +0100

    Added files for task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import sys
 import os
 
 def main():
+    """
+    Main function to handle the conversion of Markdown to HTML.
+    It validates the command line arguments and checks the existence of the input file.
+    """
     # Check if the number of arguments is less than 2
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
@@ -21,5 +25,6 @@ def main():
     # If everything is okay, exit with code 0
     sys.exit(0)
 
+# Ensure this script is not executed when imported
 if __name__ == "__main__":
     main()
